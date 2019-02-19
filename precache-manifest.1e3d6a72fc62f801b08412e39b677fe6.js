d9991d264f8950a183dda9448e0fa32535da9afd
touhidulShawan <touhidulislamshaon@gmail.com>
2019-02-19 14:56:34 +0600

    Updates

---
+++ precache-manifest.1e3d6a72fc62f801b08412e39b677fe6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "41cc247fc31b33dd0a90",
+    "url": "/Blog/static/js/runtime~main.41cc247f.js"
+  },
+  {
+    "revision": "99fb0556d3eb17fda6ca",
+    "url": "/Blog/static/js/main.99fb0556.chunk.js"
+  },
+  {
+    "revision": "2475e7db051e3ec73728",
+    "url": "/Blog/static/js/2.2475e7db.chunk.js"
+  },
+  {
+    "revision": "99fb0556d3eb17fda6ca",
+    "url": "/Blog/static/css/main.c7f8ef3c.chunk.css"
+  },
+  {
+    "revision": "55c62e6847d36d3e785c894099fdb4e6",
+    "url": "/Blog/index.html"
+  }
+];
